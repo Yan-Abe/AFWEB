4ba17b0471f43bcd9bdaa249c958d4228c57f4ea
Yan <yan.abe@hotmail.com>
2021-11-19 19:33:09 -0300

    af web

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <Systembar/>
+    <Appbar/>
+    <Navigationdrawer/>
+    <v-main >
+      <Content/>
+    </v-main>
+
+    <Footer/>
+    <Bottom/>
+  </v-app>
+  
+</template>
+
+<script>
+import Footer from './components/Footer';
+import Bottom from './components/Bottom';
+import Systembar from './components/Systembar';
+import Appbar from './components/Appbar';
+import Navigationdrawer from './components/Navigationdrawer';
+import Content from './components/Content';
+
+export default {
+  name: 'App',
+
+  components: {
+    Footer,
+    Bottom,
+    Systembar,
+    Appbar,
+    Navigationdrawer,
+    Content
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Content.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-row justify="center">
+    <v-col
+      cols="12"
+      sm="9"
+    >
+      <v-card>
+        <v-card-title class="cyan darken-1">
+          <span class="text-h5 white--text">Facens</span>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            dark
+            icon
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+
+          <v-btn
+            dark
+            icon
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+
+          <v-btn
+            dark
+            icon
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-phone</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>+55 15 3238 1188</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-phone</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>15 98154.7777</v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-email</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>facens@facens.br</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-map-marker</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Sorocaba</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-img
+          src="https://cdn.discordapp.com/attachments/911306299105505342/911382128112042054/logo-selo.jpg"
+          height="420px"
+        ></v-img>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
